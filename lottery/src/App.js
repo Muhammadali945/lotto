288a014e7d1c1bfaf56f316352028b129e608f82
Muhammadali945 <muhammadali945@gmail.com>
2023-08-01 00:03:38 +0100

    1- Fixed hacky way to save overpopulation of lottery pool.Use of hook useEffect to populate lotterypool 2- Change class for lottery balls dynamically when DOM is updated 3- winning and selected balls white color. 4- Closing info box doesnt reset game now. Better user experience

--- lottery/src/App.js
+++ lottery/src/App.js
@@ -1,43 +1,39 @@
 import React from "react";
 import "./App.css";
 import { Balls } from "./Lottery/lottery.js";
-const App = () =>
-{
+const App = () => {
   const Maxcounter = 6;
-  const [ lotteryPool, setlotteryPool ] = React.useState([]);
-  const [ counter, setCounter ] = React.useState(0);
-  const [ lottery, setLottery ] = React.useState(new Balls());
+  const [lotteryPool, setlotteryPool] = React.useState([]);
+  const [counter, setCounter] = React.useState(0);
+  const [lottery, setLottery] = React.useState(new Balls());
 
-  const lotteryPool2 = []
-  const winnerBalls = [ 0, 1, 2, 3, 4, 5 ]
-  for (let i = 1; i < 60; i++)
-  {
-    if (lotteryPool.length >= 59)
-    {
-      lotteryPool2.push(i);
-    }
-    else
-    {
-      const ball =
-      {
-        value: i,
-        selected: false
+  const winnerBalls = [0, 1, 2, 3, 4, 5]
+
+  React.useEffect(() => {
+    // Populate the lottery pool at the start of the game once
+    function populate() {
+      let result = []
+      for (let i = 1; i < 60; i++) {
+        const ball =
+        {
+          value: i,
+          selected: false
+        }
+        result.push(ball);
+        // console.log(lotteryPool.length)
       }
-      lotteryPool.push(ball);
+      setlotteryPool([...result])
     }
-  }
+    populate();
+  }, []);
 
-  function numberPressed(id)
-  {
-    if (counter === Maxcounter)
-    {
+  function numberPressed(id) {
+    if (counter === Maxcounter) {
       console.log("six balls picked...  Returning");
       return;
     }
-    let updatedPool = [ ...lotteryPool ].map((ball) =>
-    {
-      if (ball.value === id)
-      {
+    let updatedPool = [...lotteryPool].map((ball) => {
+      if (ball.value === id) {
         ball.selected = !ball.selected
       }
       return ball;
@@ -45,65 +41,47 @@ const App = () =>
     setlotteryPool(updatedPool);
   }
 
-  function toggleColor(id)
-  {
-    if (counter === 6)
-    {
+  function toggleColor(id) {
+    if (counter === 6) {
       console.log("six balls picked...  Returning");
       return;
     }
     let undo = false;
-    let updatedPool = [ ...lotteryPool ].map((ball) =>
-    {
-      if (ball.value === id && ball.selected === true)
-      {
+    let updatedPool = [...lotteryPool].map((ball) => {
+      if (ball.value === id && ball.selected === true) {
         console.log("same button pressed with id number: " + id);
         undo = true
       }
 
     });
-    if (undo)
-    {
+    if (undo) {
       let updatedCounter = counter + 1
       setCounter(updatedCounter);
-      document.getElementById(id).classList.remove('button')
-      document.getElementById(id).classList.add('button2')
       document.getElementById("number" + counter).innerHTML = id
     }
-    else
-    {
+    else {
       let updatedCounter = counter - 1
       setCounter(updatedCounter);
-      document.getElementById(id).classList.remove('button2')
-      document.getElementById(id).classList.add('button')
       document.getElementById("number" + updatedCounter).innerHTML = ""
     }
   }
 
-  function resetGame()
-  {
-    lotteryPool.map((ball) =>
-    {
-      if (ball.selected === true)
-      {
-        document.getElementById(ball.value).classList.remove('button2')
-        document.getElementById(ball.value).classList.add('button')
+  function resetGame() {
+    lotteryPool.map((ball) => {
+      if (ball.selected === true) {
+        ball.selected = false;
       }
     });
-    setlotteryPool([]);
     setCounter(0);
-    winnerBalls.map((ball) =>
-    {
+    winnerBalls.map((ball) => {
       document.getElementById("number" + ball).innerHTML = ""
     })
     document.getElementById("winning-line").innerHTML = "Pick balls and press play for draw!!!"
 
   }
 
-  async function startLottery()
-  {
-    if (counter < Maxcounter)
-    {
+  async function startLottery() {
+    if (counter < Maxcounter) {
       displayResult(7);
       return;
     }
@@ -116,36 +94,27 @@ const App = () =>
     checkWinAmount(wins);
   }
 
-  function Async()
-  {
-    return new Promise((res) =>
-    {
-      setTimeout(() =>
-      {
+  function Async() {
+    return new Promise((res) => {
+      setTimeout(() => {
         res();
       }, 3000);
     });
   }
 
-  function checkWinAmount(winningNumbers)
-  {
+  function checkWinAmount(winningNumbers) {
     let matchedNumbers = 0;
     let win = [];
     win = winningNumbers
     let selected = []
-    lotteryPool.map((ball) =>
-    {
-      if (ball.selected === true)
-      {
+    lotteryPool.map((ball) => {
+      if (ball.selected === true) {
         selected.push(ball.value);
       }
     })
-    for (let i = 0; i < win.length; i++)
-    {
-      for (let j = 0; j < selected.length; j++)
-      {
-        if (win[ i ] === selected[ j ])
-        {
+    for (let i = 0; i < win.length; i++) {
+      for (let j = 0; j < selected.length; j++) {
+        if (win[i] === selected[j]) {
           matchedNumbers++;
         }
       }
@@ -153,10 +122,8 @@ const App = () =>
     displayResult(matchedNumbers);
   }
 
-  function displayResult(matchedNumbers)
-  {
-    switch (matchedNumbers)
-    {
+  function displayResult(matchedNumbers) {
+    switch (matchedNumbers) {
       case 0:
         document.getElementById("info_text").innerHTML = "Sorry! No luck this time"
         break;
@@ -190,33 +157,31 @@ const App = () =>
     document.getElementById("popup_box").style.display = "block"
   }
 
-  function hidepopup()
-  {
+  function hidepopup() {
     document.getElementById("popup_box").style.visibility = "hidden";
-    lottery.resetNumbers();
-  }
-
-  function pickLuckyDip()
-  {
     if (counter === Maxcounter)
     {
+      lottery.resetNumbers();
+      resetGame();
+    }
+  }
+
+  function pickLuckyDip() {
+    if (counter === Maxcounter) {
       return;
     }
     resetGame();
     lottery.pickNumbers();
     let luckyNumbers = lottery.getWinNumber();
-    let updatedPool = lotteryPool.map((ball) =>
-    {
-      if (luckyNumbers.includes(ball.value))
-      {
+    let updatedPool = lotteryPool.map((ball) => {
+      if (luckyNumbers.includes(ball.value)) {
         ball.selected = true;
         toggleColor(ball.value);
       }
       return ball;
     })
     setlotteryPool(updatedPool);
-    winnerBalls.map((ball) =>
-    {
+    winnerBalls.map((ball) => {
       document.getElementById("number" + ball).innerHTML = luckyNumbers[ball]
     })
     setCounter(6);
@@ -227,8 +192,9 @@ const App = () =>
       <h1>LOTTO MILLIONAIRE</h1>
       <p>Pick six balls or try a Lucky Dip</p>
       <div class="grid-container">
-        {lotteryPool.map((ball) => <button class="button" id={ball.value} key={ball.value} onClick={() =>
-        {
+        {lotteryPool.length > 0 && lotteryPool.map((ball) => <button class="button" id={ball.value}
+         className={ ball.selected ? 'button2' : 'button' }
+          key={ball.value} onClick={() => {
           numberPressed(ball.value);
           toggleColor(ball.value);
         }}>{ball.value}</button>)}
@@ -236,29 +202,27 @@ const App = () =>
       <p>Your selected balls for the Draw</p>
       <div class="selected-balls-container">
         {
-          winnerBalls.map((ball) =>
-        {
+          winnerBalls.map((ball) => {
             return <div id={"number" + ball} key={ball} class="selected-balls">{ball.value}</div>
           })
         }
       </div>
       <div class="button-container">
-        <button class="button-28" role="button" key="bu-on" onClick={() => resetGame()}>Reset</button>
-        <button class="button-28" role="button" key="bu-on1" onClick={() => startLottery()}>Play</button>
-        <button class="button-28" role="button" key="bu-on2" onClick={() => pickLuckyDip()}>Lucky Dip</button></div>
+        <button class="button-28" key="bu-on" onClick={() => resetGame()}>Reset</button>
+        <button class="button-28" key="bu-on1" onClick={() => startLottery()}>Play</button>
+        <button class="button-28" key="bu-on2" onClick={() => pickLuckyDip()}>Lucky Dip</button></div>
       <p id="winning-line">Pick balls and press play for draw!!!</p>
       <div class="selected-balls-container">
         {
-          winnerBalls.map((ball) =>
-          {
+          winnerBalls.map((ball) => {
             return <div id={"balls" + "_" + ball} key={ball} class="win-balls"></div>
           })
         }
       </div>
       <div id="popup_box">
-        <input type="button" id="cancel_button" value="X" onClick={() => {hidepopup(); resetGame()}}></input>
+        <input type="button" id="cancel_button" value="X" onClick={() => { hidepopup(); }}></input>
         <p id="info_text">To be populated.
-        </p><input type="button" id="close_button" value="Close" onClick={() => { hidepopup(); resetGame() }}></input>
+        </p><input type="button" id="close_button" value="Close" onClick={() => { hidepopup(); }}></input>
       </div>
     </div>
   );
